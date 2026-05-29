a2e678c3023b360b90a823554bbeb2fc9acc2959
Niklas Anderson <nkanders@gmail.com>
2026-05-29 09:19:25 -0700

    Add synthesis profile for fc2 linear layer parallelism down to 8 for all
    LIF failed timing closure, the other two succeeded.

--- common/sv/neural_network.sv
+++ common/sv/neural_network.sv
@@ -299,7 +299,7 @@ module neural_network #(
         .NUM_NEURONS(HL2_SIZE),
         .NUM_TIMESTEPS(NUM_TIMESTEPS),
         .NUM_ACTIONS(NUM_ACTIONS),
-        .BATCH_SIZE(1),
+        .BATCH_SIZE(Q_BATCH_SIZE),
         .DATA_WIDTH(DATA_WIDTH),
         .MEMBRANE_WIDTH(MEMBRANE_WIDTH),
         .FRAC_BITS(FRAC_BITS),
--- common/sv/neural_network_fractional.sv
+++ common/sv/neural_network_fractional.sv
@@ -326,7 +326,7 @@ module neural_network_fractional #(
         .NUM_NEURONS(HL2_SIZE),
         .NUM_TIMESTEPS(NUM_TIMESTEPS),
         .NUM_ACTIONS(NUM_ACTIONS),
-        .BATCH_SIZE(1),
+        .BATCH_SIZE(Q_BATCH_SIZE),
         .DATA_WIDTH(DATA_WIDTH),
         .MEMBRANE_WIDTH(MEMBRANE_WIDTH),
         .FRAC_BITS(FRAC_BITS),
